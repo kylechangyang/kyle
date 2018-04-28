c78d569d1a493bde3acf84b2108fe57e4d9d899e
Kyle Yang <legokyleyang@gmail.com>
2018-04-27 23:36:48 -0700

    Update battle_the_landloard.php

--- battle_the_landloard.php
+++ battle_the_landloard.php
@@ -1,35 +1,35 @@
 <?php  
   /** 
-  * n张朴克牌随机分成4组 
+  * n pokers split into 4 groups randomly 
   * 
-  * @param int $n n张朴克牌 
+  * @param int $n n pokers 
   * @return array 
   * 
   */  
   function groupby($n)  
   {  
       $a = $b = $c = $d = array();  
-      $arr = array();         // n张有序的朴克牌  
-      $arr_about = array();   // n张无序的朴克牌  
+      $arr = array();         // n pokers in order  
+      $arr_about = array();   // n pokers in disorder 
       for ($i=1; $i < $n+1; $i++) {   
-          $arr[] = $i;    // 把朴克牌赋值给一个空的数组  
+          $arr[] = $i;    // assign poker to an empty array 
       }  
 
-      $dw_num = $n-1;     // 下标  
+      $dw_num = $n-1;     // indexing  
       while(!empty($arr)){  
 
-          $rand_number = mt_rand(0,$dw_num);  // 随机下标  
-          $arr_about[] = $arr[$rand_number];  // 把随机下标对应的值付给无序的数组  
-          unset($arr[$rand_number]);          // 清空掉这个随机下标对应的值  
-          $arr = array_values($arr);          // 去掉下标  
-          $dw_num--;                          // 下标上限降低  
+          $rand_number = mt_rand(0, $dw_num);  // random indexing
+          $arr_about[] = $arr[$rand_number];  // assign value of the random index to the disordered array 
+          unset($arr[$rand_number]);          // clear values  
+          $arr = array_values($arr);          // remove index  
+          $dw_num--;
       }  
       $per_num = count($arr_about)/4;  
-      $a = array_slice($arr_about,0,$per_num);  
-      $b = array_slice($arr_about,$per_num,$per_num);  
-      $c = array_slice($arr_about,2*$per_num,$per_num);  
-      $d = array_slice($arr_about,3*$per_num,$per_num);  
-      return array($a,$b,$c,$d);  
+      $a = array_slice($arr_about, 0, $per_num);  
+      $b = array_slice($arr_about, $per_num, $per_num);
+      $c = array_slice($arr_about, 2*$per_num, $per_num);  
+      $d = array_slice($arr_about, 3*$per_num, $per_num);  
+      return array($a, $b, $c, $d);  
   }  
   print_r(groupby(52));  
 ?>
